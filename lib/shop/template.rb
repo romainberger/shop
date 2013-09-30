55336b31250934912ae95d319c020b092598ba8e
romainberger <romain@romainberger.com>
2013-09-30 17:24:29 +0200

    Add ability to use custom templates

--- lib/shop/template.rb
+++ lib/shop/template.rb
@@ -2,15 +2,31 @@
 module Shop
   class Template
 
+    # Returns the path to the custom template if it exists
+    def custom_template_path(name)
+      config = ShopConfig.new
+      custom_path = config.get('template', 'path')
+      if File.exists?("#{custom_path}/#{name}")
+        "#{custom_path}/#{name}"
+      else
+        false
+      end
+    end
+
     # Returns the path to the templates directory
     #
     # Returns string
     def template_path(name=false)
-      path = File.expand_path File.dirname(__FILE__)
+      custom_path = custom_template_path(name)
+      if custom_path
+        custom_path
+      else
+        path = File.expand_path File.dirname(__FILE__)
 
-      return "#{path}/../../templates/#{name}" if name
+        return "#{path}/../../templates/#{name}" if name
 
-      "#{path}/../../templates"
+        "#{path}/../../templates"
+      end
     end
 
     # Replace the placeholders by the variables
@@ -20,7 +36,7 @@ module Shop
     #
     # Returns string
     def template(name, datas)
-      file = "#{template_path}/#{name}"
+      file = template_path(name)
       content = File.read(file)
 
       datas.each do |k, v|
